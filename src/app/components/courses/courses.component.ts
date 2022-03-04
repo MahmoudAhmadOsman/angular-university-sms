064e920dd81e338daa4b50fc9cc37c6197801ca6
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-03-03 23:27:58 -0600

    added course component, course service & saveStudentData function

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent implements OnInit {
+  public courses: any = [];
 
-  constructor() { }
+  constructor(private course: CourseService) {}
 
   ngOnInit(): void {
+    this.course.getCourseList().subscribe((data) => {
+      console.log(data);
+      this.courses = data;
+    });
   }
-
 }
