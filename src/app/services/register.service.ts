e1606602041f3bd359233bbbb7ae8c093f2e1247
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-03-10 23:59:11 -0600

    =added register service & updated register component

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class RegisterService {
-  public RegisterUrl = 'http://localhost:3000/users';
+  // public RegisterUrl = 'http://localhost:3000/users';
+  public RegisterUrl = 'https://custom-states-api.herokuapp.com/users';
   constructor(private http: HttpClient) {}
 
   //Register new user
