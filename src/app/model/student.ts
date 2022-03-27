b96ba9b99a6834806058cbad19b23d2f954cc6d2
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-03-26 20:21:59 -0500

    added student model class

---
+++ src/app/model/student.ts
@@ -0,0 +1,10 @@
+export interface Student {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  schoolName: string;
+  scholarship: number;
+  avatar: string;
+}
